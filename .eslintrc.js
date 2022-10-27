5acdd9c021305cc679240cac25ef8d078c98902d
Sebastian Florian <sebastian@simplefeast.com>
2022-10-27 11:33:51 +0200

    feature(Nuxt): Local setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,12 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: [
-  ],
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended'],
+  plugins: [],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'comma-dangle': ['warn', 'always-multiline'],
+  },
 }
